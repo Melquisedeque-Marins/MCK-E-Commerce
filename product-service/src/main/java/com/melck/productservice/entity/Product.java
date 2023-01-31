48d252471dfba954f518a27b8599fa5a4c795a9c
Melquisedeque <melck_junior@hotmail.com>
2023-01-31 12:57:47 +0000

    product entity created at product service

---
+++ product-service/src/main/java/com/melck/productservice/entity/Product.java
@@ -0,0 +1,27 @@
+package com.melck.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private double rate;
+    private double price;
+    private String imgUrl;
+}
