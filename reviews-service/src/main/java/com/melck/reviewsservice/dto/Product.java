249f638950c8dc9692dc59801def23ce0603add6
Melquisedeque <melck_junior@hotmail.com>
2023-02-06 16:59:36 +0000

    review service - exception handling resourceNotFoundException

---
+++ reviews-service/src/main/java/com/melck/reviewsservice/dto/Product.java
@@ -0,0 +1,23 @@
+package com.melck.reviewsservice.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    private String name;
+    private String skuCode;
+    private String description;
+    private double rate;
+    private double price;
+    private List<String> imgUrl;
+}
--- reviews-service/src/main/java/com/melck/reviewsservice/service/ReviewService.java
+++ reviews-service/src/main/java/com/melck/reviewsservice/service/ReviewService.java
@@ -1,13 +1,17 @@
 package com.melck.reviewsservice.service;
 
+import com.melck.reviewsservice.dto.Product;
 import com.melck.reviewsservice.dto.ReviewRequest;
 import com.melck.reviewsservice.dto.ReviewResponse;
 import com.melck.reviewsservice.dto.User;
 import com.melck.reviewsservice.entity.Review;
 import com.melck.reviewsservice.repository.ReviewRepository;
+import com.melck.reviewsservice.service.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -22,18 +26,33 @@ public class ReviewService {
 
     public ReviewResponse newReview(ReviewRequest reviewRequest, Long productId) {
 
+        try {
+            Product product = webClient.get()
+                    .uri("http://localhost:8080/api/v1/products/" + productId)
+                    .retrieve()
+                    .bodyToMono(Product.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            throw new ResourceNotFoundException("Product not found");
+        }
+
         Review review = mapReviewRequestToReview(reviewRequest);
         review.setProductId(productId);
-        User user = webClient.get()
-                .uri("http://localhost:8080/api/v1/users/" + reviewRequest.getUserId())
-                .retrieve()
-                .bodyToMono(User.class)
-                .block();
-        String[] name = user.getFullName().split(" ");
-        reviewRepository.save(review);
-        ReviewResponse response = mapReviewToReviewResponse(review);
-        response.setUserName(name[0]);
-        return response;
+
+        try {
+            User user = webClient.get()
+                    .uri("http://localhost:8080/api/v1/users/" + reviewRequest.getUserId())
+                    .retrieve()
+                    .bodyToMono(User.class)
+                    .block();
+            String[] name = user.getFullName().split(" ");
+            reviewRepository.save(review);
+            ReviewResponse response = mapReviewToReviewResponse(review);
+            response.setUserName(name[0]);
+            return response;
+        } catch (WebClientResponseException e) {
+            throw new ResourceNotFoundException("User not found");
+        }
     }
 
     public List<ReviewResponse> getAllReviewByProduct(Long productId) {
