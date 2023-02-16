8cb076924a3f8c98f106643d3b9bbcd797a738fe
Melquisedeque <melck_junior@hotmail.com>
2023-02-16 10:58:51 +0000

    review service - circuitbreaker and fallBackMethods

--- reviews-service/src/main/java/com/melck/reviewsservice/controller/ReviewController.java
+++ reviews-service/src/main/java/com/melck/reviewsservice/controller/ReviewController.java
@@ -2,9 +2,10 @@ package com.melck.reviewsservice.controller;
 
 import com.melck.reviewsservice.dto.ReviewRequest;
 import com.melck.reviewsservice.dto.ReviewResponse;
-import com.melck.reviewsservice.entity.Review;
 import com.melck.reviewsservice.service.ReviewService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/reviews")
@@ -20,6 +22,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/product/{Id}")
+    @CircuitBreaker(name = "user",  fallbackMethod = "fallbackMethod")
     public ResponseEntity<ReviewResponse> newReview(@RequestBody ReviewRequest reviewRequest, @PathVariable Long Id) {
         ReviewResponse newReview = reviewService.newReview(reviewRequest, Id);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newReview.getId()).toUri();
@@ -27,8 +30,19 @@ public class ReviewController {
     }
 
     @GetMapping("/{productId}")
+    @CircuitBreaker(name = "user",  fallbackMethod = "userFallbackMethod")
     public ResponseEntity<List<ReviewResponse>> getAllReviewByProduct(@PathVariable Long productId) {
         List<ReviewResponse> reviews = reviewService.getAllReviewByProduct(productId);
         return ResponseEntity.ok(reviews);
+    }
+
+    public ResponseEntity<String> fallbackMethod(Exception e) {
+        log.info("Oops! Something went wrong, the user service or product service is down. Please try again later.", e);
+        return ResponseEntity.ok().body("Oops! Something went wrong, Please try again later.");
+    }
+
+    public ResponseEntity<String> userFallbackMethod(Exception e) {
+        log.info("Oops! Something went wrong, the user service is down. Please try again later.", e);
+        return ResponseEntity.ok().body("Oops! Something went wrong, Please try again later.");
     }
 }
