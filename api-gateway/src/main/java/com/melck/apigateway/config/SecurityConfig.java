151e2b306e38c5a9bfee0eb0086fd6c3980deee6
Melquisedeque <melck_junior@hotmail.com>
2023-02-22 20:46:08 +0000

    product service - resource server config

--- api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.melck.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,19 +12,13 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+    public SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
 
-        httpSecurity
-                .csrf().disable()
-                .authorizeExchange((exchange) -> exchange
-                        .pathMatchers("/eureka/**").permitAll()
-                        .anyExchange()
-                        .authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        httpSecurity
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.GET, "/api/v1/products/**")
-                .permitAll();
         return httpSecurity.build();
     }
 }
+
---
+++ product-service/src/main/java/com/melck/productservice/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.melck.productservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//    private String issuerUri;
+//
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return JwtDecoders.fromIssuerLocation(issuerUri);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}
