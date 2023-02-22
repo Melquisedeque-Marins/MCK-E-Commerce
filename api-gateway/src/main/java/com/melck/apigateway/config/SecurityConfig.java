c31b57c1f1215e11a8d6d97a54ecb726ceec6144
Melquisedeque <melck_junior@hotmail.com>
2023-02-22 17:41:20 +0000

    api gateway - oauth client config

--- api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.melck.apigateway.config;
 
-import jakarta.ws.rs.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,27 +11,20 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    public static final String ADMIN = "admin";
-    public static final String USER = "user";
-
-
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
 
-        serverHttpSecurity.csrf()
-                .disable()
-                .authorizeExchange(exchange -> exchange
+        httpSecurity
+                .csrf().disable()
+                .authorizeExchange((exchange) -> exchange
                         .pathMatchers("/eureka/**").permitAll()
-//                        .pathMatchers("/api/v1/products/**").hasRole(ADMIN)
-                        .pathMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
-//                        .pathMatchers(HttpMethod.GET, "/api/v1/users/**").hasAnyRole(ADMIN, USER)
-                        .pathMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/api/v1/users").hasRole(ADMIN)
-                        .pathMatchers(HttpMethod.POST, "/api/v1/cart").permitAll()
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
-
+        httpSecurity
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.GET, "/api/v1/products/**")
+                .permitAll();
+        return httpSecurity.build();
     }
 }
