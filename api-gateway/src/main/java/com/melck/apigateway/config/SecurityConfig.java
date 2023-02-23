f2649a77287a5d9542c8dbcdda67f3a52f5a916c
Melquisedeque <melck_junior@hotmail.com>
2023-02-23 14:02:18 +0000

    api gateway - authorization release for product endpoints

--- api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/melck/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.melck.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
                         .anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults());
         httpSecurity.csrf().disable();
--- product-service/src/main/java/com/melck/productservice/config/SecurityConfig.java
+++ product-service/src/main/java/com/melck/productservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.melck.productservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -26,6 +27,7 @@ public class SecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
