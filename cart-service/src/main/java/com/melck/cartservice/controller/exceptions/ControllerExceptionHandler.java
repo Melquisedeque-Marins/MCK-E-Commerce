3e64e08839d83f3887a0babfd0641d472b48ec12
Melquisedeque <melck_junior@hotmail.com>
2023-03-06 11:19:13 +0000

    cart service - validation and exception handling

---
+++ cart-service/src/main/java/com/melck/cartservice/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.melck.cartservice.controller.exceptions;
+
+import com.melck.cartservice.service.exception.CartNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    Instant now = Instant.now();
+    @ExceptionHandler(CartNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(CartNotFoundException e, HttpServletRequest request){
+
+        StandardError error = new StandardError(now, HttpStatus.NOT_FOUND.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError error = new ValidationError(now, HttpStatus.BAD_REQUEST.value(), request.getRequestURI() , "Field validation error - please check the fields above");
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
