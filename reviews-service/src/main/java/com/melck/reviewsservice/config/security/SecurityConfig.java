c37186d65195c27dcc78edfadc4757a8b87be2cc
Melquisedeque <melck_junior@hotmail.com>
2023-03-07 17:10:56 +0000

    review service - add security config

---
+++ reviews-service/src/main/java/com/melck/reviewsservice/config/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.melck.reviewsservice.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig  {
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/v1/reviews/**").permitAll()
+                        .requestMatchers( "/swagger-resources/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasRole(ADMIN)
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+//    @Bean
+//    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+//        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+//            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+//            Collection<String> roles = realmAccess.get("roles");
+//            return roles.stream()
+//                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+//                    .collect(Collectors.toList());
+//        };
+//
+//        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+//
+//        return jwtAuthenticationConverter;
+//    }
+}
