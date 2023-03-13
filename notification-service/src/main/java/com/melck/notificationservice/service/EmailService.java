248e82f907a14e18a35a6adc5afd70a4723bf8c4
Melquisedeque <melck_junior@hotmail.com>
2023-03-13 20:52:02 +0000

    notification service - add rabbitMQ and create listener to user created queue

--- notification-service/src/main/java/com/melck/notificationservice/service/EmailService.java
+++ notification-service/src/main/java/com/melck/notificationservice/service/EmailService.java
@@ -1,11 +1,13 @@
 package com.melck.notificationservice.service;
 
+import com.melck.notificationservice.dto.User;
 import com.melck.notificationservice.entity.Email;
 import com.melck.notificationservice.enums.StatusEmail;
 import com.melck.notificationservice.repository.EmailRepository;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.MailException;
@@ -22,27 +24,28 @@ public class EmailService {
     private EmailRepository repository;
     private JavaMailSender mailSender;
 
-    public Email sendEmail() {
+    @RabbitListener(queues = "users.v1.user-created")
+    public Email sendEmail(User user) {
 
-//        String name = user.getName();
-//        String[] firstName = name.split(" ");
-//        String q = firstName[0];
+        String name = user.getFullName();
+        String[] firstName = name.split(" ");
+        String userName = firstName[0];
 
         Email email = new Email();
         email.setSendDateEmail(LocalDateTime.now());
-        email.setEmailTo("melck_junior@hotmail.com");
-        email.setOwnerRef("user");
+        email.setEmailTo(user.getEmail());
+        email.setOwnerRef(userName);
 
         email.setSubject("Account registered successfully");
         email.setEmailFrom("mck.enterprises.clinic@gmail.com");
-        email.setText("<h1>Olá, <style='text-transform:capitalize;'>" + "user" + "</h1>"
+        email.setText("<h1>Olá, <style='text-transform:capitalize;'>" + userName + "</h1>"
                 + "\n Tou are now part of MCK-E-Commerce family. \n"
                 + "<img src='https://www.feedz.com.br/blog/wp-content/uploads/2021/10/mensagem-de-boas-vindas-para-novos-colaboradores-1.webp' alt='teste'/> ");
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setFrom(email.getEmailFrom());
-            helper.setTo("melck_junior@hotmail.com");
+            helper.setTo(user.getEmail());
             helper.setSubject(email.getSubject());
             helper.setText(email.getText(),true);
             helper.addAttachment("equipe.jpg", new ClassPathResource("/static/img/equipe.jpg"));
