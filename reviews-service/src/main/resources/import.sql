39fbfe0b699b59fdfe288e8b3fbbfacdfaf08655
Melquisedeque <melck_junior@hotmail.com>
2023-05-01 19:40:22 +0100

    updating database sead

---
+++ reviews-service/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 1, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 2, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 2, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 3, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 3, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 2, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 2, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 4, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 5, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 6, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 7, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+INSERT INTO tb_review (user_id, product_id, title, description, rate, created_at )  VALUES (1, 8, 'teste', 'ótimo produto, recomendo', 5.0, NOW());
+
